12d03827cf4e899d13f25429e9249fabd0468d82
DDD <Sabitov.daniyar18@mail.ru>
2021-09-16 14:22:05 +0300

    Started manager

--- src/Realisation/FootballManager.java
+++ src/Realisation/FootballManager.java
@@ -7,7 +7,7 @@ import java.util.*;
 
 
 public class FootballManager {
-    public List<String> teamParser(String path) {
+    private List<String> teamParser(String path) {
         List<String> teams = new ArrayList<>();
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
@@ -19,15 +19,34 @@ public class FootballManager {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        System.out.println(teams);
         return teams;
     }
 
-    private Map<String, String> couples(Set<String> teams) {
+    private Map<String, String> couples(List<String> teams) {
         Random random = new Random();
-        Map<String, String> couples = null;
-        for (int i = 0; i < teams.size() / 2; i++) {
-            couples.put(String.valueOf(teams.remove(random.nextInt(teams.size()))),String.valueOf(teams.remove(random.nextInt(teams.size()))));
+        Map<String, String> couples = new HashMap<>();
+        System.out.println(teams.size());
+        for (int i = 0; i < teams.size(); i++) {
+            couples.put(String.valueOf(teams.remove(random.nextInt(teams.size()))), String.valueOf(teams.remove(random.nextInt(teams.size()))));
         }
-        return null;
+        return couples;
+    }
+
+    private Map<String, String> goals(Map<String, String> couples) {
+        Random random = new Random();
+        System.out.println(couples);
+        HashMap<String, String> totalGoals = new HashMap<>();
+        for (String couple : couples.keySet()) {
+            totalGoals.put(couple + " - " + couples.get(couple), random.nextInt(7) + ":" + random.nextInt(7));
+        }
+        return totalGoals;
+    }
+
+    public Map<String, String> start(String path) {
+        FootballManager footballManager = new FootballManager();
+        List<String> teams = footballManager.teamParser(path);
+        Map<String, String> totalGoals = footballManager.couples(teams);
+        return footballManager.goals(totalGoals);
     }
 }
--- src/Realisation/Main.java
+++ src/Realisation/Main.java
@@ -7,13 +7,10 @@ import java.util.HashSet;
 public class Main {
     public static void main(String[] args) {
         FootballManager footballManager = new FootballManager();
-        System.out.println(footballManager.teamParser("C:\\Users\\Пользователь\\JavaFiles\\untilted\\DoborExamInfo\\src\\Data\\Teams"));
-        HashSet<String> hashSet = new HashSet<>();
-        hashSet.add("lol");
-        hashSet.add("kek");
-        hashSet.add("ch");
-        ArrayList<String> k = new ArrayList<>();
-        k.add("k");
-        System.out.println(String.valueOf(k.remove(0)));
+        System.out.println(footballManager.start("C:\\Users\\Пользователь\\JavaFiles\\untilted\\DoborExamInfo\\src\\Data\\Teams"));
+        ArrayList<String> p = new ArrayList<>();
+        p.add("k");
+        p.remove(0);
+        System.out.println(p);
     }
 }
