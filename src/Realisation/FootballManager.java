a329fb3d7f5c6b3902fb6af317f63c62a6f32dda
DDD <Sabitov.daniyar18@mail.ru>
2021-09-16 11:18:58 +0300

    Started manager

--- src/Realisation/FootballManager.java
+++ src/Realisation/FootballManager.java
@@ -3,23 +3,31 @@ package Realisation;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+
 
 public class FootballManager {
-public List<String> teamParser(String path){
-    ArrayList<String> teams = new ArrayList<>();
-    try {
-        BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
-        String line;
-        while ((line= bufferedReader.readLine())!=null){
-            String[] atributes=line.split(",");
-            teams.addAll(Arrays.asList(atributes));
+    public List<String> teamParser(String path) {
+        List<String> teams = new ArrayList<>();
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] atributes = line.split(",");
+                teams.addAll(Arrays.asList(atributes));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-    }  catch (IOException e) {
-        e.printStackTrace();
+        return teams;
     }
-    return teams;
-}
+
+    private Map<String, String> couples(Set<String> teams) {
+        Random random = new Random();
+        Map<String, String> couples = null;
+        for (int i = 0; i < teams.size() / 2; i++) {
+            couples.put(String.valueOf(teams.remove(random.nextInt(teams.size()))),String.valueOf(teams.remove(random.nextInt(teams.size()))));
+        }
+        return null;
+    }
 }
--- src/Realisation/Main.java
+++ src/Realisation/Main.java
@@ -1,8 +1,19 @@
 package Realisation;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Main {
     public static void main(String[] args) {
         FootballManager footballManager = new FootballManager();
         System.out.println(footballManager.teamParser("C:\\Users\\Пользователь\\JavaFiles\\untilted\\DoborExamInfo\\src\\Data\\Teams"));
+        HashSet<String> hashSet = new HashSet<>();
+        hashSet.add("lol");
+        hashSet.add("kek");
+        hashSet.add("ch");
+        ArrayList<String> k = new ArrayList<>();
+        k.add("k");
+        System.out.println(String.valueOf(k.remove(0)));
     }
 }
